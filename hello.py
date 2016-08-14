4d05fec9b1db7a1e960bdb55419ec5bcf6aeb797
hellocomrade <sun8726@hotmail.com>
2016-08-14 13:32:37 -0400

    update test case

--- hello.py
+++ hello.py
@@ -4,20 +4,21 @@ from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 import tables
 
+
 class TestAlchemy(unittest.TestCase):
     def setUp(self):
         self.engine = sqlalchemy.create_engine('sqlite:///:memory:', echo = True)
-	self.Session = sessionmaker(bind = self.engine)
+        self.Session = sessionmaker(bind = self.engine)
     
     @contextmanager
     def getSession(self):
-	try:
-	    session = self.Session()
+        session = self.Session()
+        try:
             yield session
-	    session.commit()
-	except:
-	    session.rollback()
-	    raise
+            session.commit()
+        except:
+            session.rollback()
+            raise
         finally:
             session.close()
     
@@ -28,12 +29,20 @@ class TestAlchemy(unittest.TestCase):
         self.assertEqual(sqlalchemy.__version__, "1.0.14", "SQLAlchemy version is different than 1.0.14")
     
     def test_tables(self):
-	tables.CreateAllTables(self.engine)
+        tables.CreateAllTables(self.engine)
         self.assertEqual(self.engine.dialect.has_table(self.engine, tables.User.__tablename__), True, "Table User doesn't exist")
-	#session doesn't implement __enter__ and __exit__
-	with self.getSession() as session:
-	    session.add(tables.User(name = "foo", password = "bar"))
-	    self.assertEqual(session.query(tables.User).filter_by(name = 'foo').first().password, 'bar', "User info doesn't match with input")
+        #session doesn't implement __enter__ and __exit__
+        with self.getSession() as session:
+            session.add(tables.User(name = "foo", password = "bar"))
+            self.assertEqual(session.query(tables.User).filter_by(name = 'foo').first().password, 'bar', "User info doesn't match with input")
+            self.assertEqual(session.query(tables.User).filter_by(name = 'bar').first(), None, "User bar should not exist")
+            session.add(tables.User(name = "bar", password = "bar"))
+            session.add(tables.User(name = "foobar", password = "bar"))
+            result = session.query(tables.User).filter_by(password = "bar").order_by(tables.User.name)
+            print len(result)
+            for r in result:
+                print r
+            result.close()
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestAlchemy)
 unittest.TextTestRunner(verbosity = 2).run(suite)
